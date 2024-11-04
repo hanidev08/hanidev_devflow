9ab0c9e5ba2a1cccef4e941423cb3aa922246f0b
hanidev08 <hanidev08@gmail.com>
2024-11-04 20:36:50 +0100

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with Google and GitHub Providers

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navigation/navbar";
+import React from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <Navbar />
+      {children}
+    </main>
+  );
+};
+
+export default RootLayout;
